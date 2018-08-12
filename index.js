f0ec06ccb6287ffd1a81dc452f7de61d779d5f07
tinluong28@aol.com <tinluong28@aol.com>
2018-08-12 15:22:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,21 +24,11 @@ function dec(a){
   return `${(a -= 1)}`
 }
 
-function makeInt(n) {
-  return `${(parseInt(n))}`
-}
 
 function makeInt(n) {
   return `${(parseInt(n,10))}`
 }
 
-function makeInt(n) {
-  return `${(parseInt(n,10))}`
-}
-
-function preserveDecimal(n) {
-  return `${parseFloat(n)}`
-}
 
 function preserveDecimal(n) {
   return `${(parseFloat(n))}`
